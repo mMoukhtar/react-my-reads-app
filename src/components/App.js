00bc06f6becd5abe555ba61ddd5af5ed818b0a83
mMoukhtar <mohamed.moukhtar@outlook.com>
2021-02-08 22:39:58 +0200

    FIX: mMdify the Book Shelf update Logic

--- src/components/App.js
+++ src/components/App.js
@@ -18,34 +18,37 @@ class BooksApp extends Component {
     // Question #1:
     // I am using this arrow function and passing it down multiple level to Book child components!
     // Is this correct or should I use different approach?
+    // Question #2:
+    // Is there a better way to write the below code?
     changeShelf = (changes) => {
         const { oldBook, newShelf } = changes;
-
-        this.setState((oldState) => {
-            const oldShelfBooksWithoutChangedBook = oldState[oldBook.shelf].filter(
-                (book) => book.id !== oldBook.id
-            );
-            if (newShelf !== 'none') {
-                return {
-                    ...oldState,
-                    [newShelf]: [
-                        ...oldState[newShelf],
-                        {
-                            ...oldBook,
-                            shelf: newShelf,
-                        },
-                    ],
-                    [oldBook.shelf]: [...oldShelfBooksWithoutChangedBook],
-                };
-            } else {
-                return {
-                    ...oldState,
-                    [oldBook.shelf]: [...oldShelfBooksWithoutChangedBook],
-                };
-            }
+        const { id: oldBookId, shelf: oldShelf } = oldBook;
+        // Question #3:
+        // When should I call the API? before I update the state or after updating it?
+        // Also should I use any component event to handle the Update API call?
+        BooksAPI.update(oldBook, newShelf).then((data) => {
+            this.setState((oldState) => {
+                const oldShelfBooksAfterUpdate = oldState[oldShelf].filter((book) => book.id !== oldBookId);
+                const shelfExists = Object.keys(oldState).includes(newShelf);
+                const containsBooks = oldShelfBooksAfterUpdate.length > 1;
+                // Create new State Object
+                //1. Delete changed shelf from
+                delete oldState[oldShelf];
+                //2. Copy old object without changed shelf
+                let newState = { ...oldState };
+                //3. If selected shelf not none
+                //   check if the shelf is none and that shelf exists if doesn't exist create new shelf
+                newShelf !== 'none' && (newState[newShelf] = newState[newShelf] ? newState[newShelf] : []);
+                //   Push book to shelf
+                newState[newShelf].push({
+                    ...oldBook,
+                    shelf: newShelf,
+                });
+                // 4. If old shelf still cotains books add the updated books to new state object
+                containsBooks && (newState[oldBook.shelf] = [...oldShelfBooksAfterUpdate]);
+                return newState;
+            });
         });
-
-        BooksAPI.update(oldBook, newShelf);
     };
 
     groupByShelf = (books) =>
